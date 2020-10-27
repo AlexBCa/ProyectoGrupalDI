ce1e44080252263053936617750ebf80896ea12d
Add <Add@DESKTOP-2ADOEMC>
2020-10-27 19:34:48 +0100

    Añadido boton y colores

--- ProyectoFXGrupal/src/Controller/Controller.java
+++ ProyectoFXGrupal/src/Controller/Controller.java
@@ -1,17 +1,41 @@
 package Controller;
 
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class Controller {
 
     @FXML
     private Label label;
+    @FXML
+    private Label label2;
+    
+    ArrayList<Color> colores = new ArrayList<Color>();
+    
+    
+    public void cambiarColor(ActionEvent event) {
+    	Random r = new Random();
+    	label2.setTextFill(colores.get(r.nextInt(4)));
+    	label.setTextFill(colores.get(r.nextInt(4)));
+    }
 
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        
+        colores.add(Color.RED);
+        colores.add(Color.GREEN);
+        colores.add(Color.MAGENTA);
+        colores.add(Color.TURQUOISE);
+        colores.add(Color.PURPLE);
+        
+    
     }
 }
